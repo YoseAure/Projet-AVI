db3838d3b9ad7d2c5b1b53b565252c6f53e62ef5
Alan Huguel <alanhuguel@MacBook-Air-de-Alan.local>
2025-05-26 09:56:22 +0200

    Ajout des consignes pour PA et Mini-manche

--- lecture_plan.py
+++ lecture_plan.py
@@ -47,11 +47,20 @@ def generate_legs_strings(waypoints):  # générer les legs au format chaîne de
         legs_strings.append(leg_str)
     return legs_strings
 
+def generate_z_consigne(waypoints) :
+    """
+    Génère une liste des altitudes de consigne à partir des waypoints.
+    """
+    z_consigne = []
+    for wp in waypoints:
+        z_consigne.append(wp['z'])
+    return z_consigne
+
 if __name__ == "__main__":
     # Utiliser le nom de fichier 'fpl.txt'
     waypoints = read_waypoints('fpl.txt')
     
-    # Afficher le résultat
+    """# Afficher le résultat
     print(f"\nNombre de waypoints lus : {len(waypoints)}\n")
     
     coordinates = []
@@ -61,13 +70,17 @@ if __name__ == "__main__":
     
     print("Liste des coordonnées :")
     for coord in coordinates:
-        print(coord)
+        print(coord)"""
     
     fplwpts_string = generate_fplwpts_string(waypoints)
-    print("\nPlan de vol :")
+    print("Plan de vol :")
     print(fplwpts_string)
 
-    legs = generate_legs(waypoints)
+    z_consigne = generate_z_consigne(waypoints)
+    print("\nAltitudes de consigne:")
+    print("|".join(str(z) for z in z_consigne))
+
+    """legs = generate_legs(waypoints)
     for idx, (start, end) in enumerate(legs, 1):
         print(f"\nLeg {idx}:")  # Format : name,x,y,z,overfly
         print(f"{start['name']},{start['x']},{start['y']},{start['z']},{start['overfly']}") # Waypoint de départ
@@ -76,4 +89,4 @@ if __name__ == "__main__":
     legs_strings = generate_legs_strings(waypoints)
     print("\nLegs formatés :")
     for idx, leg_str in enumerate(legs_strings, 1):
-        print(f"Leg {idx}: {leg_str}")
+        print(f"Leg {idx}: {leg_str}")"""
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Commande dans le terminal pour lancer le script = ./aircraft-sim-darwin-10.10-amd64 -bus 10.1.127.255:2049
 from ivy.std_api import *
-from lecture_plan import read_waypoints, generate_fplwpts_string
+from lecture_plan import read_waypoints, generate_fplwpts_string,generate_z_consigne
 FCU_On = False
 Mode_PA_Longitudinale = "Managed"
 
 waypoints = read_waypoints("fpl.txt")
 fplwpts_string = generate_fplwpts_string(waypoints)
+
+vitesse_consigne = 250.0 # Vitesse de consigne en nœuds
+altitude_consigne = generate_z_consigne(waypoints) # Altitude de consigne en pieds
 
 def on_Push(agent, *larg):   #*larg = tableau qui contient les argument éventuels de notre message
     global FCU_On
@@ -18,9 +21,35 @@ def on_Push(agent, *larg):   #*larg = tableau qui contient les argument éventue
     else:
         FCU_On = False
         IvySendMsg("FCUAP1 off")
-
+        
     print("Message recu du FCU")
-    IvySendMsg(f"FplWpts Waypoints={fplwpts_string}") # Envoi d'un message de type FplWpts
+    IvySendMsg(f"FplWpts Waypoints={fplwpts_string}") # Afficher le plan de vol
+
+#------------Mini Manche------------------
+
+    #ANGLE DE ROULIS PHHI
+    IvySendMsg("FGS_PHI_MAX_MANCHE=66.0")
+
+    #FACTEUR DE CHARGE N
+    IvySendMsg("FGS_N_MAX_MANCHE=2.5")
+    IvySendMsg("FGS_N_MIN_MANCHE=-1.5")
+
+    #VITESSE DE ROULIS P
+    IvySendMsg("FGS_P_MAX_MANCHE=15.0")
+
+#------------PA LONGITUDINAL---------------
+
+    #PHI
+    IvySendMsg("FGS_PHI_MAX=33.0")
+    IvySendMsg("FGS_PHI_MIN=-33.0")
+
+    #VITESSE DE CONSIGNE
+    IvySendMsg(f"FGS_V_CONSIGNE={vitesse_consigne}")
+
+    #ALTITUDE DE CONSIGNE
+    IvySendMsg(f"FGS_Z_CONSIGNE={altitude_consigne}")  
+
+#------------------------------------------
 
 def on_FCUVertical(agent, *larg):
     global Mode_PA_Longitudinal
